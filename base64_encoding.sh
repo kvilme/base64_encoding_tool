39f05c327f244a90d157599ce04788e96f699d38
kvilme <34861726+kvilme@users.noreply.github.com>
2018-03-06 18:43:29 -0500

    Add files via upload
    base64 encoding tool for linux.

---
+++ base64_encoding.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#This script base64 encode a userId and UserPwd. 
+
+#Read and cCreate a variable to hold userInput
+read -p "Enter Your userName: " userName
+
+# Check if userName is empty
+if [ -z ${userName} ]; then
+    echo -e "\n You must enter a userName"
+    exit 1
+else 
+   read -p "Enter Your userPassword: " userPassword
+
+   # Check if userPassword is empty
+   if [ -z ${userPassword} ]; then
+      echo -e "\n You must enter a userName"
+      exit 1
+   fi
+fi
+
+
+echo -e "\n $userName\n"
+echo -e "\n $userPassword\n"
+
+# Base64 encode apmUserName and apmUserpassword 
+echo -e "\nbase64_Encoded_String: "
+echo -n "$userName:$userPassword" | base64 -w 0 
+echo -e  "\n"
+
+#cho "base64_Encoded_String: ${base64_Encoded_String}"
